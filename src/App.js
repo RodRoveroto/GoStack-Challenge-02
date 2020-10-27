c417dc10b8cb4e076e2f8d05263b5ebaa8aea3f7
Rod <rodolforoveroto@gmail.com>
2020-10-27 14:55:34 -0300

    Solved

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import api from "./services/api";
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositorie] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(res => {
+      setRepositorie(res['data']);
+    })
+  }, [])
+  
+
   async function handleAddRepository() {
-    // TODO
+    const repositorie = {
+      title: `Mobile com React native ${Date.now()}`,
+      url: "google.com",
+      techs: ["Angular"]
+    }
+    const res = await api.post('repositories', repositorie)
+
+    const result = res.data;
+    setRepositorie([...repositories ,result])
   }
 
-  async function handleRemoveRepository(id) {
-    // TODO
+  async function handleRemoveRepository(id, index) {
+    await api.delete('/repositories/' + id).then(() => {
+      repositories.splice(index, 1)
+      setRepositorie([...repositories])
+    })
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map((repositorie, index) => {
+          return (
+            <li key={repositorie.id}>
+              {repositorie.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+              <button onClick={() => handleRemoveRepository(repositorie.id, index)}>
+                Remover
           </button>
-        </li>
+            </li>
+          )
+        })}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
